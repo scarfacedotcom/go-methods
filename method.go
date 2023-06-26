3a5a82b5d15b6256a700cbc5a1e61e02a3be272c
scarfacedotcom <peteradaaku@gmail.com>
2023-06-26 21:43:44 +0100

    println

--- method.go
+++ method.go
@@ -1,5 +1,29 @@
 package main
 
+import "fmt"
+
+type Rectangle struct {
+	width, height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.width * r.height
+}
+
+func (r *Rectangle) Scale(factor float64) {
+	r.width *= factor
+	r.height *= factor
+}
+
 func main() {
+	rect := Rectangle{width: 10, height: 5}
+
+	// Value receiver method call
+	area := rect.Area()
+	fmt.Println(area)
+
+	// Pointer receiver method call
+	rect.Scale(2)
+	fmt.Println(rect)
 
 }
